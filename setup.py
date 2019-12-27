90f2f53f3eb33481518c9dded29cdca075601677
shikano.takeki <distortion0mix@gmail.com>
2019-12-27 16:10:48 +0900

    build packages

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Learn more: https://github.com/kennethreitz/setup.py
 
 from setuptools import setup, find_packages
-
 
 with open('README.rst') as f:
     readme = f.read()
@@ -17,7 +15,7 @@ setup(
     description='client tool for managing s3 operation.',
     long_description=readme,
     author='takeki shikano',
-    author_email='shikano.takeki@nexon.co.jp',
+    author_email='',
     url='',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
